f9c3040ef2a4d36fc56ffd62a90976f7aa86d189
ime520 <32449442+ime520@users.noreply.github.com>
2018-03-06 19:42:40 +0800

    Add some attributes and functions.

--- Slider.js
+++ Slider.js
@@ -5,13 +5,21 @@
         o1[i]=o2[i];
     }
   }
+  function html2node(html){
+    var container=document.createElement('div');
+    container.innerHTML=html;
+    return container.children[0];//html只能有一个根节点
+  }
   var template=`<div class='m-slider'></div>`;
   function Slider(options){
-    extend(this,options);
+    extend(this,options);//this增加属性：images-存放图片位置的数组，interval-自动播放的时间，container-要将轮播图插入到哪个dom元素
+    this.slider=html2node(template);
+    this.imgLength=this.images.length;
+    this.init();
   }
   Slider.prototype.nav=function(index){
     if(this.index===index)return;
-    this.last=this.index;//'this'增加了一个last属性，存放之前的索引位置
+    this.last=this.index;//this增加了一个last属性，存放之前的索引位置
     this.index=index;
     this.fade();
     this.setCurrent();
