7c88f675ec43459870c1ebc664d25961ffb1e1c8
ime520 <32449442+ime520@users.noreply.github.com>
2018-03-06 19:16:25 +0800

    Add a function for copying
    Assign the incoming parameters to 'this'.

--- Slider.js
+++ Slider.js
@@ -1,6 +1,13 @@
 (function(App){
+  function extend(o1,o2){
+    for(var i in o2){
+      if(o1[i]==undefined)
+        o1[i]=o2[i];
+    }
+  }
   var template=`<div class='m-slider'></div>`;
-  function Slider(){
+  function Slider(options){
+    extend(this,options);
   }
   App.Slider=Slider;
 })(window.App);
